4e082f9cd198f157e1571d3e739993adbc2fd470
Annika Shogren <annika.shogren@gmail.com>
2011-05-22 14:02:53 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ly2::Application.config.secret_token = '98c439c442c2f3d227db41de07f0a08305d80b8045af87e84f880fceb299faa073e053ad50cf40a6a169d5e3578cea92619c89d599775f58e9d293fecacb6ddf'
